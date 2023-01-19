e958ad972ae2471a8843511a916b0724e8771dfe
wagbo12 <115502757+wagbo12@users.noreply.github.com>
2023-01-19 15:32:34 -0500

    added new files for menu -  Williams

--- Restaurant_Api/Models/Menu.cs
+++ Restaurant_Api/Models/Menu.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using MongoDB.Bson;
+
 namespace Restaurant_Api.Models
 {
 	public class Menu
 	{
 		public Menu()
 		{
+			//The Item Id
+        	public ObjectId _id { get; set; }
+
+			//check menu items to display
+			public List<MenuItem>? FoodList { get; set; }
+
 		}
 	}
 }
